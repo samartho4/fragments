2a9c62fa56c755056f89eef9138b365b369619d2
samartho4 <142962370+samartho4@users.noreply.github.com>
2025-01-09 18:38:22 -0500

    Updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
 ];
