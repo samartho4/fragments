e09f4f9f625482ba776f48d8f883da1e1e8e4c07
samartho4 <142962370+samartho4@users.noreply.github.com>
2025-03-17 21:03:14 -0400

    refactor: improve get API

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,18 +1,12 @@
 // src/routes/api/get.js
 const logger = require('../../logger');
 const { Fragment } = require('../../model/fragment');
-const { createSuccessResponse, createErrorResponse } = require('../../response'); // Import the function
+const { createSuccessResponse, createErrorResponse } = require('../../response');
 
-module.exports = (req, res) => {
-  // TODO: Replace this placeholder with actual logic
-  const fragments = []; // Placeholder for fragments data
-  res.status(200).json(createSuccessResponse({ fragments })); // Use createSuccessResponse
-};
 /**
  * Get a list of fragments for the current user
  */
-// ===== Get a list of fragments for the current user =====
-module.exports.getFragmentsList = async (req, res) => {
+async function getFragmentsList(req, res) {
   try {
     // Fetch the expanded list of objects if the user opts for it
     if (req.query.expand === '1') {
@@ -42,4 +36,8 @@ module.exports.getFragmentsList = async (req, res) => {
     logger.error('An error occurred while fetching the list of fragments for user:', error.message);
     res.status(500).send(createErrorResponse(500, 'Internal Server Error'));
   }
+}
+
+module.exports = {
+  getFragmentsList
 };
