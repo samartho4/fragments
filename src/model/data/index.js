3e429a356471d92bec756d000aff49f71d3487d5
samartho4 <142962370+samartho4@users.noreply.github.com>
2025-04-15 01:41:15 -0400

    updateed as form discussion

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,6 +1,8 @@
 // src/model/data/index.js
+// eslint-disable-next-line no-unused-vars
 const s3Client = require('./aws/s3Client');
+// eslint-disable-next-line no-unused-vars
 const ddbDocClient = require('./aws/ddbDocClient');
 
-// Export the AWS implementation
-module.exports = require('./aws');
+
+module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
